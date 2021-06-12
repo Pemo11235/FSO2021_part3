ca54f670ea51378aea230cd8c9a48e8714f772ee
pemo11235 <matteoluceri@msn.com>
2021-06-12 21:45:45 +0200

    Added 3.3

--- index.js
+++ index.js
@@ -29,14 +29,30 @@ let persons = [
 app.get('/', (request, response) => {
     response.send('<h1>Hello world!</h1>')
 })
+
 app.get('/api/persons',
     (request, response) => {
         response.json(persons)
-    })
+    }
+)
 
-let personsLength = persons.length;
-let timestamp = new Date();
-app.get('/info' , (request,response) => {
+app.get('/api/persons/:id',
+    (request, response) => {
+        const id = Number(request.params.id)
+        const person = persons.find(person => person.id === id)
+
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end();
+        }
+    }
+)
+
+
+app.get('/info', (request, response) => {
+    const personsLength = persons.length;
+    const timestamp = new Date();
     response.send(`<p>Phonebook has info for ${personsLength} people.</p> 
 <p>${timestamp}</p>`)
 
