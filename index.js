42182dc39a3669573791312cead93a665dfe9a8a
pemo11235 <matteoluceri@msn.com>
2021-06-19 23:18:38 +0200

    Added 3.8

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const {request} = require("express");
 const app = express()
 
 app.use(express.json())
+
 var morgan = require('morgan')
-app.use(morgan('tiny'))
+const {format, token} = require("morgan");
+morgan.token('body', (req, res) => (JSON.stringify(req.body)))
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
 
 let persons = [
     {
